8895d094288f3aa648fc57955cf1837cca75ba16
gilbertasiimwe-ctrl <asimgilbs744@gmail.com>
2025-09-15 17:04:11 +0300

    calculator_submission1

--- calculator/main.c
+++ calculator/main.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main(){
+    // DisplayMenue
+    printf("====== Simple Clculator======");
+    printf("\n1. Addition(+)");
+    printf("\n2. Subtraction(-)");
+    printf("\n3. Multiplication(*)");
+    printf("\n4. Division(/)");
+    printf("\n5. Modulus(%%)");
 
-int main()
-{
-    char operator;
-    printf("Choose an Operator ['+','-','*','/']:");
-    scanf("%c", &operator);
-    double num1, num2;
-    printf("Enter first number: ");
-    scanf("%lf", &num1);
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
+
+    int operator = 0;
+    double num1,num2;
     double result;
+    printf("\nEnter operator(1-(+) 2-(-) 3-(*) 4-(/) 5-(%%):");
+    scanf("%d", &operator);
 
-    return 0;
+    printf("Enter num1:\n");
+    scanf("%lf", &num1);
+
+    printf("Enter num2:\n");
+    scanf("%lf", &num2);
+
+    switch(operator){
+        case 1:
+            result = num1 + num2;
+            printf("result: %.2lf", result);
+            break;
+        case 2:
+            result = num1 - num2;
+            printf("result: %.2lf", result);
+            break;
+        case 3:
+            result = num1 * num2;
+            printf("result: %.2lf", result);
+            break;
+        case 4:
+            if (num2 == 0){
+                printf("Division by zero is not allowed!");
+            }
+            else{
+                result = num1 / num2;
+                printf("result: %.2lf", result);
+            }
+            break;
+        case 5:
+            if (num2 == 0){
+                printf("Modulus by zero is not allowed!");
+            }
+            else{
+                result = (int)num1 % (int)num2;
+                printf("result: %d", (int)result);
+            }
+            break;
+        default:
+            printf("The operator is invalid!!", operator);
+    }
+
+
+
 }
